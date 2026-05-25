0c6e551c906c1cf1b9b5665c370859e5bb0bd2bc
MrMed <techmrmed@gmail.com>
2026-05-26 00:32:38 +0530

    added be url in config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // Use the deployed backend URL by default
-export const BACKEND_URL = process.env.NODE_ENV === 'development' 
-  ? "http://localhost:3000" 
-  : "https://codedock-0g3k.onrender.com"
+export const BACKEND_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://genos-be.vercel.app";
